948cee739a8ff7c56fc9b492795e9b695d820715
Mike Harris <mharris717@gmail.com>
2014-02-01 10:38:26 -0500

    Initial

---
+++ lib/odds.rb
@@ -0,0 +1,59 @@
+require 'mharris_ext'
+
+class Odds
+  include FromHash
+  attr_accessor :win_chance
+
+  def to_s
+    if win_chance < 0.5
+      "+" + (win_amount_without_principal * 100.0).to_i.to_s
+    else
+      n = (1.0 / loss_chance) - 1.0
+      "-" + (n * 100.0).to_i.to_s
+    end
+  end
+
+  def win_amount_with_principal
+    1.0 / win_chance
+  end
+  def win_amount_without_principal
+    win_amount_with_principal - 1.0
+  end
+  def loss_chance
+    1.0 - win_chance
+  end
+  
+  def to_s_to_one
+    if win_chance < 0.5
+      "#{win_amount_without_principal} to 1"
+    else
+      "1 to #{(1.0 / win_amount_without_principal).round(1)}"
+    end
+  end
+
+  def *(other)
+    res = win_chance * other.win_chance
+    self.class.new(:win_chance => res)
+  end
+    
+  def inverse
+    self.class.new(:win_chance => (1.0 - win_chance))
+  end
+    
+  class << self
+    def from_string(str)
+      sign = str[0..0]
+      num = str[1..-1].to_f / 100.0
+      if sign == '-'
+        from_win_amount_without_principal(1.0 / num)
+      elsif sign == '+'
+        from_win_amount_without_principal(num)
+      else
+        raise "bad #{str}"
+      end
+    end
+    def from_win_amount_without_principal(num)
+      new(:win_chance => (1.0 / (1.0 + num)))
+    end
+  end
+end
--- spec/odds_spec.rb
+++ spec/odds_spec.rb
@@ -1,7 +1,63 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Odds" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  def self.should_have_to_s(win_chance,exp)
+    it "to_s #{win_chance} -> #{exp}" do
+      odds = Odds.new(:win_chance => win_chance)
+      odds.to_s.should == exp
+    end
+  end
+  
+  def odds(win_chance)
+    Odds.new(:win_chance => win_chance)
+  end
+
+  should_have_to_s 0.25, '+300'
+  should_have_to_s 0.75, '-300'
+  should_have_to_s 0.4, '+150'
+  should_have_to_s 0.6, '-150'
+
+  it 'win_amount_with_principal' do
+    odds(0.25).win_amount_with_principal.should == 4
+  end
+
+  it 'win_amount_with_principal' do
+    odds(0.75).win_amount_with_principal.round(3).should == 1.333
+  end
+
+  it 'win_amount_with_principal' do
+    odds(0.9).win_amount_with_principal.round(3).should == 1.111
+  end
+  
+  it 'to_s_to_one' do
+    odds(0.25).to_s_to_one.should == "3.0 to 1"
+  end
+  
+  it 'to_s_to_one' do
+    odds(0.75).to_s_to_one.should == "1 to 3.0"
+  end
+  
+  describe 'multiply' do
+    it 'smoke' do
+      (odds(0.5) * odds(0.5)).win_chance.should == 0.25
+    end
+  end
+  
+  it 'inverse' do
+    odds(0.25).inverse.win_chance.should == 0.75
+  end
+  
+  describe "other creation methods" do
+    it 'from string' do
+      Odds.from_string("+300").win_chance.should == 0.25
+    end
+    
+    it 'from string' do
+      Odds.from_string("-300").win_chance.should == 0.75
+    end
+    
+    it 'from string bad' do
+      lambda { Odds.from_string("42") }.should raise_error
+    end
   end
 end

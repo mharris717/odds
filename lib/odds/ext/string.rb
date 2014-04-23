8c54fde4bea1c3be5defd4174866d9dade0d4bc0
Mike Harris <mharris717@gmail.com>
2014-04-23 13:01:50 -0400

    to_s_perc tweak

--- lib/odds/ext/string.rb
+++ lib/odds/ext/string.rb
@@ -1,5 +1,6 @@
 class Numeric
   def to_s_perc(digits=2)
+    return "nan" if to_s.downcase == 'nan'
     n = self.to_f * 100.0
     n.round(digits).to_s + "%"
   end
@@ -11,5 +12,11 @@ class Numeric
     n = n.to_s
     n << "0" if places == 1
     "#{sign}$#{n}"
+  end
+end
+
+class NilClass
+  def to_s_perc(*args)
+    nil
   end
 end
